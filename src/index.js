cd749d8c53f5053bf968e9491ad61be61892059f
21tarun <tarunuday2000@gmail.com>
2023-01-21 17:52:10 +0530

    modification in css

--- src/index.js
+++ src/index.js
@@ -27,7 +27,6 @@ io.on('connection',function(socket){
 
     socket.on('message',function(msg){
         console.log(msg)
-        history.push(msg)
         
         socket.broadcast.emit('message',msg)
     })
